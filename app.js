c01ce40c5b903e3af18ac69447c64591be341b43
jkhiba2023 <khan.jabir1055@gmail.com>
2025-02-03 20:42:00 +0530

    Adding new elements

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 const button = document.querySelector('button');
 const h1 = document.querySelector('h1');
 
-button.addEventListener('click', function () {
-  const newColor = makeRandomColor();
-  document.body.style.backgroundColor = newColor;
-  h1.innerText = newColor;
-})
-
 const makeRandomColor = () => {
   const r = Math.floor(Math.random() * 255);
   const g = Math.floor(Math.random() * 255);
   const b = Math.floor(Math.random() * 255);
   return `rgb(${r}, ${g}, ${b})`;
 }
+
+button.addEventListener('click', function () {
+  const newColor = makeRandomColor();
+  document.body.style.backgroundColor = newColor;
+  h1.innerText = newColor;
+})
+
+h1.addEventListener('click', function () {
+  h1.style.color = makeRandomColor();
+})
+
+
+const buttonBoxs = document.querySelectorAll('.boxes');
+
+for (let btnBox of buttonBoxs) {
+  btnBox.addEventListener('click', function () {
+    btnBox.style.backgroundColor = makeRandomColor();
+    btnBox.style.color = makeRandomColor();
+  })
+}
+
